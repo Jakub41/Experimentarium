d6a56f86de51f2cc94e17669c46d7d1612f9d912
Jakub41 <lemiszewski@gmx.com>
2017-03-28 21:10:17 +0200

    Deleted Gruntfile

--- GruntTest/Gruntfile.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (grunt) {
-    grunt.registerTask("default", "", function () {
-        grunt.log.write("This grunt task is pointless!!!");
-
-        // Time how long tasks take. Can help when optimizing build times
-        require('time-grunt')(grunt);
-    });
-};
