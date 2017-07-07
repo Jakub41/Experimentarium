0a4dfb9d387bc20e1875d8bd107ebcf4f8766914
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-07-07 12:29:16 +0200

    ES6 continue...

---
+++ ES6/ES6Tut-1.0-NoBabel/letVSvar.js
@@ -0,0 +1,48 @@
+"use strict"
+// Var VS Let 
+// var a = "test 1";
+// let b = "test 2";
+
+//console.log(a + " " + b);
+
+// function testVar() {
+//     var a = 30;
+//     if (true) {
+//         var a = 50;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+
+// testVar();
+
+// console.log("--------------------");
+
+// function testLet() {
+//     let a = 30;
+//     if (true) {
+//         let a = 50;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+
+// testLet();
+
+// console.log("--------------------");
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// console.log("--------------------");
+
+// Const
+
+// const colors = [];
+
+// colors.push("red");
+// colors.push("blue");
+
+// console.log(colors);
