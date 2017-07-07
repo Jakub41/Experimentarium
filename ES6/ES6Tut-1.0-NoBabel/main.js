78f4c49d4502113fc8950803e78caf55f12b4fdb
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-07-07 14:36:53 +0200

    ES6 crashing in

--- ES6/ES6Tut-1.0-NoBabel/main.js
+++ ES6/ES6Tut-1.0-NoBabel/main.js
@@ -1,39 +1,13 @@
 "use strict"
 
-class User {
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+let name = "Jake";
 
-    static countUsers() {
-        console.log("There are 50 users");
-    }
-
-    register() {
-        console.log(this.username + " is now registered");
-    }
+function makeUppercase(word) {
+    return word.toUpperCase();
 }
 
-// let bob = new User("bob", "bob@mail.com", "12345");
+let template =
+    `<h1>${makeUppercase("My Own Template")} by ${name}</h1> 
+    <p>This is my simple template in ES6 Javascript</p>`;
 
-// bob.register();
-
-User.countUsers();
-
-class Member extends User {
-    constructor(username, email, password, memberPackage) {
-        super(username, email, password);
-        this.package = memberPackage;
-    }
-
-    getPackage() {
-        console.log(this.username + " has this package " + this.package);
-    }
-}
-
-let mike = new Member("Mike", "mike@email.com", "12345", "standard");
-
-mike.getPackage();
-mike.register();
+document.getElementById('template').innerHTML = template;
---
+++ ES6/ES6Tut-1.0-NoBabel/string.js
@@ -0,0 +1,24 @@
+"use strict"
+
+let theString = "Working on this awesome template";
+
+// startsWith()
+// endsWith()
+// includes()
+
+console.log(theString.startsWith("Working"));
+console.log(theString.endsWith("template"));
+console.log(theString.includes("Not awesome"));
+
+// Hex
+console.log(0xFF);
+
+// Binary
+console.log(0b101011);
+
+// Octal
+console.log(0o543);
+
+// Numbers
+console.log(Number.isFinite(NaN)); //isNaN() -> true
+console.log(Number.isInteger(45));
